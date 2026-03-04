82628085331c04a0853da5a69a1beb6dd5eb1519
Salman Qureshi <er.salmanqureshi@gmail.com>
2026-03-04 23:47:23 +0400

    Initial Commit

---
+++ RealTimePriceTracker/RealTimePriceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealTimePriceTrackerApp.swift
+//  RealTimePriceTracker
+//
+//  Created by Salman Qureshi on 3/4/26.
+//
+
+import SwiftUI
+
+@main
+struct RealTimePriceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
